42d1dbd0bdc30435bb9e6b408afd53a16f38387a
clone <lehir-dylan@hotmail.com>
2024-06-11 14:54:04 +0200

    Initial Commit

---
+++ poc_orange_belt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  poc_orange_belt
+//
+//  Created by Dylan Le Hir on 11/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ poc_orange_belt/poc_orange_beltApp.swift
@@ -0,0 +1,17 @@
+//
+//  poc_orange_beltApp.swift
+//  poc_orange_belt
+//
+//  Created by Dylan Le Hir on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct poc_orange_beltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
